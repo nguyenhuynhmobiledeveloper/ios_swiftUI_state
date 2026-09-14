cdeb2a9ac548c6b319f40d5377cb90a46be89f77
MobileDev <nguyenhuynh.mobiledeveloper@gmail.com>
2026-09-14 21:30:52 +0700

    Initial Commit

---
+++ ios_swiftUI_state/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_swiftUI_state
+//
+//  Created by MobileDev on 14/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_swiftUI_state/ios_swiftUI_stateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swiftUI_stateApp.swift
+//  ios_swiftUI_state
+//
+//  Created by MobileDev on 14/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_swiftUI_stateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
